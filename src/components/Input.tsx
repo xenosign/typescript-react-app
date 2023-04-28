4a96448de1689cfa675e5c2f7c393a697ad0fa0f
xenosign <xenosign@naver.com>
2023-04-28 13:10:48 +0900

    12

---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import React, { useState, useRef } from "react";
+
+export default function Input() {
+  const [string, setString] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleChange = (): void => {
+    if (inputRef.current !== null) setString(inputRef.current.value);
+  };
+
+  return (
+    <>
+      <h1>{string}</h1>
+      <input ref={inputRef} onChange={handleChange} />
+    </>
+  );
+}
